8decee87ddc0c17cea09d0397b235ffc1b688072
Phillip Henry III <philtrader71@gmail.com>
2020-01-03 18:07:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,5 @@
 def find_element_index(array, value_to_find)
-counter = 0
 
-  while counter << array.length do
-    puts array[value_to_find]
-    counter += 1
-  end
 end
 
 def find_max_value(array)
